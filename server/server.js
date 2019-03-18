a3a5e7aed1c789e99332564e652f7389c08cdc80
pjay79 <praveen.jayarajan@gmail.com>
2019-03-18 14:10:42 +1100

    Minor edit to server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
-const app = express();
 const bodyParser = require('body-parser');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
