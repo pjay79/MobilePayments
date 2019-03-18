3e679fff01422eae7bed08c89c5c812941986414
pjay79 <praveen.jayarajan@gmail.com>
2019-03-18 14:11:31 +1100

    Minor edit to server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
