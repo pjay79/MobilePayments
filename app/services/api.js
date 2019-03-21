076fb472c7f95f6d9689ff98a5f440f545f0c1b4
pjay79 <praveen.jayarajan@gmail.com>
2019-03-21 23:52:07 +1100

    Make payments with aws lambda

--- app/services/api.js
+++ app/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Config from 'react-native-config';
 
 const makePayment = async (amount, tokenId) => {
   try {
@@ -9,7 +10,8 @@ const makePayment = async (amount, tokenId) => {
     const headers = {
       'Content-Type': 'application/json',
     };
-    const data = await axios.post('http://localhost:5000/api/payments', body, { headers });
+    // const data = await axios.post(Config.DEV_URL, body, { headers });
+    const data = await axios.post(Config.PROD_URL, body, { headers });
     return data;
   } catch (error) {
     console.log('Error in making payment', error);
